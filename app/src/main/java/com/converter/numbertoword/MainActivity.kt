8c75096db1ecd8f6a9d2820066aa427abe12fcc4
2020-android-6456dev <kaustubh.panchal@neebal.com>
2020-02-08 12:15:57 +0530

    Number to word function definition added, UI restrictions added for validation also added validation in converter class

--- app/src/main/java/com/converter/numbertoword/MainActivity.kt
+++ app/src/main/java/com/converter/numbertoword/MainActivity.kt
@@ -1,14 +1,67 @@
 package com.converter.numbertoword
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.converter.convert.getNumberInWords
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.converter.utils.getDecimalFromString
+import com.converter.utils.getNumberInWords
 
 class MainActivity : AppCompatActivity() {
+
+    var tvNumberInWords: TextView? = null
+    var etNumberInput: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        getNumberInWords(12)
+
+        initViews()
+        init()
+        setUpListeners()
     }
+
+    private fun init() {
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    private fun setUpListeners() {
+        etNumberInput?.addTextChangedListener(TextChangeListener())
+    }
+
+    private fun initViews() {
+        tvNumberInWords = findViewById(R.id.tv_number_in_words)
+        etNumberInput = findViewById(R.id.et_number_input)
+    }
+
+    inner class TextChangeListener : TextWatcher {
+        override fun afterTextChanged(s: Editable?) {
+            if (s.isNullOrEmpty()) {
+                tvNumberInWords?.text = getString(R.string.txt_label_number_in_words)
+            } else {
+
+                try {
+                    val num = getDecimalFromString(s.toString())
+                    tvNumberInWords?.text = getNumberInWords(num)
+                } catch (e: IllegalArgumentException){
+                    tvNumberInWords?.text = getString(R.string.txt_error_number_validation)
+                }
+
+            }
+        }
+
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+        }
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        finish()
+        return true
+    }
+
 }
--- app/src/main/java/com/converter/utils/ConverterUtils.kt
+++ app/src/main/java/com/converter/utils/ConverterUtils.kt
@@ -1,9 +1,76 @@
 package com.converter.utils
 
+val units = arrayOf(
+    "", "One", "Two", "Three", "Four",
+    "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve",
+    "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen",
+    "Eighteen", "Nineteen"
+)
+
+val tens = arrayOf(
+    "",
+    "",
+    "Twenty",
+    "Thirty",
+    "Forty",
+    "Fifty",
+    "Sixty",
+    "Seventy",
+    "Eighty",
+    "Ninety"
+)
+
+/**
+ * This function converts number to words
+ * @number input number
+ */
 fun getNumberInWords(number: Int): String {
-    throw IllegalArgumentException()
+
+    //Number related errors handled here
+    if(number > 999999){
+        throw IllegalArgumentException()
+    }
+
+    if (number < 0) {
+        throw IllegalArgumentException()
+    }
+
+    if (number < 20) {
+        return units[number]
+    }
+
+    if (number < 100) {
+        return tens[number / 10] + (if (number % 10 != 0) " " else "") + units[number % 10]
+    }
+    if (number < 1000) {
+        return units[number / 100] + " Hundred" + (if (number % 100 != 0) " " else "") + getNumberInWords(
+            number % 100
+        )
+    }
+    if (number < 100000) {
+        return getNumberInWords(number / 1000) + " Thousand" + (if (number % 10000 != 0) " " else "") + getNumberInWords(
+            number % 1000
+        )
+    }
+    return if (number < 10000000) {
+        getNumberInWords(number / 100000) + " Lakh" + (if (number % 100000 != 0) " " else "") + getNumberInWords(
+            number % 100000
+        )
+    } else getNumberInWords(number / 10000000) + " Crore" + (if (number % 10000000 != 0) " " else "") + getNumberInWords(
+        number % 10000000
+    )
 }
 
+/**
+ * This function converts number in string format to integer
+ * @numberText takes number input in string format and returns decimal
+ */
 fun getDecimalFromString(numberText: String): Int{
-    throw IllegalArgumentException()
+    try {
+        return numberText.trim().toInt()
+    } catch (e: Exception){
+        // Number related errors handled here
+        // Works for any number conversion error, we can show error with message for different cases if required
+        throw IllegalArgumentException()
+    }
 }
